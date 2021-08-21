d28193137b8232aea8a9fa8eca11feeea6d37369
shashvati46 <67509222+shashvati46@users.noreply.github.com>
2021-08-21 18:16:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("getTimeZone")
+if(button!==null){
+button.addEventListener("click, timeZone")
+}
+
+function timezone(){
+    document.getElementById("showTimeZone").innerHTML = Date()
+
+}
